3eb30494d63193cccfbb50edd88465b38c02e7d5
Priscila Veras <vrspriscila@gmail.com>
2021-06-29 02:55:28 +0100

    not complete

---
+++ src/FormFood.js
@@ -0,0 +1,15 @@
+import {Component} from ".react";
+
+
+
+class FormFood extends Component{
+    render(
+    return(
+<div class="control">
+  <input class="input is-hovered" type="text" placeholder="Hovered input">
+</div>
+
+))
+}
+
+export default FormFood
